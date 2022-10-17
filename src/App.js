5d398efba9291f22c214b599929672904cdf81dc
Lộc Đặng <kendidang04082k@gmail.com>
2022-10-18 00:08:42 +0700

    set up lib,UI of face card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
+import Webcam from 'react-webcam';
+import { FaceMesh } from '@tensorflow-models/facemesh';
 
 function App() {
+
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  const runFacemess = async () => {
+    const net = await FaceMesh.load({
+      inputResolution: { width: '700px', height: '700px' },
+      scale: 0.8,
+    });
+    setInterval(() => {
+      detect(net)
+    }, 100);
+  }
+
+  const detect = async (net) => {
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readState === 4) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces(video)
+      console.log("🚀 ~ file: App.js ~ line 32 ~ detect ~ face", face)
+
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam className='webcam' ref={webcamRef} />
+      <canvas className='canvas' ref={canvasRef} />
     </div>
   );
 }
